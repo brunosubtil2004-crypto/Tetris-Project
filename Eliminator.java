fd9e66a1ad304a36141ab318eac3a9747cbbb257
Bruno Subtil <brunosubtil2004@gmail.com>
2025-11-29 02:49:22 +0000

    Created Eliminator Interface

---
+++ Eliminator.java
@@ -0,0 +1,17 @@
+
+
+/** Interface que define o comportamento de eliminação de sequências. */
+public interface Eliminator {
+
+    /**
+     * Procura uma sequência de símbolos contíguos no array seq e substitui-os pelo símbolo nothing.
+     * * @param seq O array de símbolos onde procurar.
+     * @param blockSize O tamanho mínimo da sequência para ser eliminada.
+     * @param nothing O símbolo a usar para substituir os eliminados.
+     * @return O número de símbolos eliminados.
+     */
+
+    int eliminateSequence(Symbol[] seq, int blockSize, Symbol nothing);
+
+    
+}
